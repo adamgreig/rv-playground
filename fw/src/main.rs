172efe67d63b6be9fd329c00f519e7202d6bc0b6
Adam Greig <adam@adamgreig.com>
2022-01-09 23:26:28 +0000

    Use unreleased new riscv crate's asm::delay instead of home-made delay function

--- fw/src/main.rs
+++ fw/src/main.rs
@@ -5,15 +5,6 @@
 // This one just goes into an infinite loop on panic.
 use panic_halt as _;
 
-// Simple delay function that delays for some multiple of n clock cycles.
-fn delay(n: usize) {
-    for _ in 0..n {
-        // There's no "nop" in the risc-v crate, so just do a dummy volatile
-        // memory read instead, which the compile may not elide.
-        unsafe { core::ptr::read_volatile(0 as *const u32) };
-    }
-}
-
 #[riscv_rt::entry]
 fn main() -> ! {
     // Our custom hardware maps address 0x2000_0000 to the 32-bit GPO (general-purpose output)
@@ -21,10 +12,12 @@ fn main() -> ! {
     // the address and do volatile writes to it.
     let gpo = 0x2000_0000 as *mut u32;
 
-    loop {
-        unsafe { gpo.write_volatile(1) };
-        delay(1_000_000);
-        unsafe { gpo.write_volatile(0) };
-        delay(1_000_000);
+    unsafe {
+        loop {
+            gpo.write_volatile(1);
+            riscv::asm::delay(1_000_000);
+            gpo.write_volatile(0);
+            riscv::asm::delay(1_000_000);
+        }
     }
 }
